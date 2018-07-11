c64500debf06c1a3c91f804834345e941ea7b735
Mel Michael <mel.michael@andela.com>
2018-07-11 15:03:59 +0100

    Implement wrapper for the redis client

---
+++ index.js
@@ -0,0 +1,9 @@
+import redis from 'redis';
+import bluebird from 'bluebird';
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+const dataClient = redis.createClient();
+
+export default dataClient;
