3f9a10623883c173ea99574215e48b3cdb350029
salvatore-fxpig <salvatore@fxpig.com>
2019-10-01 14:08:49 +0200

    Add "safe" hset functions

--- index.js
+++ index.js
@@ -12,10 +12,22 @@ const config = {
 Bluebird.promisifyAll(redis.RedisClient.prototype);
 Bluebird.promisifyAll(redis.Multi.prototype);
 
+redis.RedisClient.prototype.hsetAsyncSafe = function (...args) {
+  if ((args || []).some(arg => arg === undefined || arg === null))
+    console.error("BAD HSET INPUT", ...args);
+  return this.hsetAsync(...args);
+};
+
+redis.Multi.prototype.hsetSafe = function (...args) {
+  if ((args || []).some(arg => arg === undefined || arg === null))
+    console.error("BAD HSET INPUT", ...args);
+  return this.hset(...args);
+};
+
 const subscriber = redis.createClient(config);
 const dataClient = redis.createClient(config);
 
-dataClient.on("error", err =>console.log("RESDIS dataClient error:", err));
-subscriber.on("error", err =>console.log("REDIS subscriber error:", err));
+dataClient.on("error", err => console.log("REDIS dataClient error:", err));
+subscriber.on("error", err => console.log("REDIS subscriber error:", err));
 
 module.exports = { dataClient, subscriber }
